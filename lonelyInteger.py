98628c10b51d3b8d191875f3922fe37a62f542bd
Afrasiyab <afrasiyabmanzoir709@gmail.com>
2023-01-09 10:40:41 +0500

    Add files via upload

---
+++ lonelyInteger.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    a.sort()
+    i = 0
+    length = len(a)
+    while i+1 < length:
+        if a[i] != a[i+1]:
+            return a[i]
+        i += 2
+    return a[-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
